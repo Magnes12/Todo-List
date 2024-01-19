1033dd433b6f7f648a4f0a792ebbe0201ec51b8e
Damian <m4gn3s@gmail.com>
2024-01-19 12:00:31 +0100

    TODO

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '123456789'
+tasks_list = []
 
 
-@app.route("/")
+class ToDo(FlaskForm):
+    tasks = StringField('what to do', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route("/", methods=["POST", "GET"])
 def home():
-    return render_template('index.html')
+    form = ToDo()
+    if form.validate_on_submit():
+        task = form.tasks.data
+        tasks_list.append(task)
+    return render_template('index.html', form=form, tasks=tasks_list)
 
 
 if __name__ == "__main__":
